34d093f541913d0065e33d6302f1ab1863d7636a
박정민 <v5833165v@naver.com>
2023-10-11 14:31:27 +0900

    데이터추가 수정

--- make_data.py
+++ make_data.py
@@ -16,7 +16,7 @@ def make_user():
         {
             "user_id": "test1",
             "user_password": "password1",
-            "user_gender": "Male",
+            "user_gender": "M",
             "user_birth": "1990-01-01",
             "user_url": "https://park-dev-diary.tistory.com/",
             "user_lastpost": "",
@@ -25,7 +25,7 @@ def make_user():
         {
             "user_id": "test2",
             "user_password": "password1",
-            "user_gender": "Male",
+            "user_gender": "M",
             "user_birth": "1990-01-01",
             "user_url": "https://study4silver.tistory.com/",
             "user_lastpost": "",
@@ -34,7 +34,7 @@ def make_user():
         {
             "user_id": "test3",
             "user_password": "password1",
-            "user_gender": "FeMale",
+            "user_gender": "W",
             "user_birth": "1990-01-01",
             "user_url": "https://dev-dobim.tistory.com/",
             "user_lastpost": "",
@@ -83,53 +83,6 @@ def make_user():
     collection.insert_many(userpost_data)
 
 
-
-
-
-
-    
-
-
-    
-
-def insert_all():
-    # URL을 읽어서 HTML를 받아오고,
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get('https://movie.daum.net/ranking/boxoffice/yearly', headers=headers)
-
-    html_tree = etree.HTML( data.text )
-    movie_list = html_tree.xpath("//ol[@class='list_movieranking']/li")
-
-    for movie_tree in movie_list:
-
-        title = movie_tree.xpath(".//a[@class='link_txt']")[0].text
-        link = movie_tree.xpath(".//a[@class='link_txt']/@href")[0]
-        open_date = movie_tree.xpath(".//span[@class='info_txt'][1]/span" )[0].text
-        people = movie_tree.xpath(".//span[@class='info_txt'][2]")[0]
-        people = [ t for t in people.itertext()][1]
-        poster_url = movie_tree.xpath(".//img/@src")[0]
-
-        open_year, open_month, open_day = map(int,open_date.split('.'))
-        open_year += 2000
-
-        link = 'https://movie.daum.net' + link
-         
-        viewers =  ''.join(filter(str.isdigit, people))
-        
-        doc = { 
-            'title': title,
-            'open_year': open_year,
-            'open_month': open_month,
-            'open_day': open_day,
-            'viewers': int(viewers),
-            'poster_url' : poster_url,
-            'trashed' : False,
-            'like' : 0,
-            'link' : link
-        }   
-
-        db.movies.insert_one(doc)
-        print('완료: ', title, open_year, open_month, open_day, viewers)
 
 if __name__ == '__main__':
     # # 기존의 movies 콜렉션을 삭제하기
