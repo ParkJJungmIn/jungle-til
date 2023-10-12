837e9996563cb2646245d2953e4ae6ae92cd63f5
진상하 <98368934+slemdem@users.noreply.github.com>
2023-10-12 18:08:35 +0900

    Update app_instance.py

--- app_instance.py
+++ app_instance.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from flask_jwt_extended import *
 
 app = Flask(__name__)
 
+# 토큰 생성에 사용될 Secret Key를 flask 환경 변수에 등록
+app.config.update(
+			DEBUG = True,
+			JWT_SECRET_KEY = "I'M IML"
+		)
+
+# JWT 확장 모듈을 flask 어플리케이션에 등록
+jwt = JWTManager(app)
+
+def get_jwt():
+    return jwt
 
