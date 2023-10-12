4455fc3f9d8462ffc431b0d7c2a9716cac93fd3f
진상하 <98368934+slemdem@users.noreply.github.com>
2023-10-12 18:06:19 +0900

    Update login.py

--- login.py
+++ login.py
@@ -2,24 +2,22 @@ from bson import ObjectId
 from pymongo import MongoClient
 import requests
 from lxml import etree
-from flask import Flask, render_template, jsonify, request, render_template_string
+from flask import Flask, render_template, jsonify, request, render_template_string, make_response
 from flask.json.provider import JSONProvider
 from pprint import pprint
 import json
 import sys
 
-import jwt
-import datetime
 import hashlib
 
-from app_instance import app
-from mongo_setup import get_db
+from app_instance import *
+from mongo_setup import get_db, get_secret_key
 
 db = get_db()
 
 #JWT 토큰을 만들기 위해 필요한 비밀문자열 - 아무거나 입력해도 된다
 #서버 내에만 저장되기에 이 서버 내에서만 인코딩 디코딩 할 수 있게 해준다
-SECRET_KEY = 'JUNGLE'
+SECRET_KEY = get_secret_key()
 
 @app.route('/login', methods=['GET'])
 def get_login():    return render_template('login.html')
@@ -78,7 +76,7 @@ def log_in():
     password_encrypted = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     
     #DB에서 아이디와 암호화된 비밀번호로 회원 조회
-    checker = db.user.find_one({"user_id":id_receive},{'user_password':password_encrypted})
+    checker = db.user.find_one({"user_id":id_receive}, {'user_password':password_encrypted})
     
     #DB에서 조회된 값이 있다면
     if checker is not None:
@@ -86,40 +84,10 @@ def log_in():
         # 시크릿키가 있어야 토큰을 디코딩하여 payload 값을 확인 할 수 있음
         # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
         # exp에는 만료시간을 넣어줍니다(5초). 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
-        payload = {
-            'id': id_receive,
-            'exp': datetime.datetime.now() + datetime.timedelta(hours=1)
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-        return jsonify({'result': 'success', 'token' : token})
+        access_token = create_access_token(identity = id_receive,
+											expires_delta = False)
+        
+        return jsonify({'result': 'success', 'access_token' : access_token})
     
     else:
         return jsonify({'result': 'fail'})
-
-#토큰 유효성 검사
-def check_token(token):
-    try: 
-        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        
-        #얼마 안남았을 때 30분 이하
-        if payload['exp'] + datetime.timedelta(minutes=30)< datetime.datetime.now():
-            refreshed = refresh_token(token)
-            return refreshed
-    except jwt.ExpiredSignatureError:
-        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
-        return False
-    except jwt.exceptions.DecodeError:
-        return False
-    
-    #아무 이상없다면
-    return True
-
-def refresh_token(token):
-        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        newpayload = {
-            'id': payload['id'] ,
-            'exp': datetime.datetime.now() + datetime.timedelta(hours=1)
-        }
-        newtoken = jwt.encode(newpayload, SECRET_KEY, algorithm='HS256')
-        return newtoken
-    
